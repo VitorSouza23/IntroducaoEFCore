797bba3bae1f9a0ee70736a281306a385aa0b830
Vitor de Souza <vitordesolza@gmail.com>
2020-11-10 18:08:56 -0300

    Verificando se há migrações pendentes

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using IntroducaoEFCore.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace IntroducaoEFCore
 {
@@ -6,6 +9,13 @@ namespace IntroducaoEFCore
     {
         static void Main(string[] args)
         {
+            using ApplicationContext db = new ApplicationContext();
+
+            db.Database.GetPendingMigrations()
+            .ToList()
+            .ForEach(m => Console.WriteLine(m));
+            //db.Database.Migrate();
+
             Console.WriteLine("Hello World!");
         }
     }
