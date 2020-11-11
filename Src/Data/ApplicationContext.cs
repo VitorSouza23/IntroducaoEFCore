2e51615ff2e6bc2045a93b1ed298014478d51959
Vitor de Souza <vitordesolza@gmail.com>
2020-11-11 09:26:53 -0300

    Inserindo método que adiciona mais de um tipode de dado no banco

--- Src/Data/ApplicationContext.cs
+++ Src/Data/ApplicationContext.cs
@@ -1,18 +1,22 @@
 using IntroducaoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace IntroducaoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
-
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
         public DbSet<Order> Orders {  get; set ; }
         public DbSet<Product> Products {  get; set ; }
         public DbSet<Customer> Customers {  get; set ;} 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;initial Catalog=IntroducaoEFCore;Integrated Security=true");
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Data source=(localdb)\\mssqllocaldb;initial Catalog=IntroducaoEFCore;Integrated Security=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Src/Program.cs
+++ Src/Program.cs
@@ -18,7 +18,8 @@ namespace IntroducaoEFCore
             .ForEach(m => Console.WriteLine(m));
             //db.Database.Migrate();
 
-            InsertData();
+            //InsertData();
+            InsertDataRange();
             Console.WriteLine("Hello World!");
         }
 
@@ -37,6 +38,31 @@ namespace IntroducaoEFCore
             applicationContext.Products.Add(product);
             applicationContext.SaveChanges();
             Console.WriteLine($"Produto {product.GetHashCode()} inserido na base.");
+        }
+
+        private static void InsertDataRange()
+        {
+            Product product = new Product()
+            {
+                Description = "Test",
+                BarCode = "1234567",
+                Value = 10m,
+                ProductType = ProductType.MerchandiseForResale,
+                Actived = true
+            };
+
+            Customer customer = new Customer()
+            {
+                CEP = "999999",
+                City = "Teste",
+                Name = "Teste",
+                UF = "AA",
+                Phone = "1111111"
+            };
+
+            using ApplicationContext applicationContext = new ApplicationContext();
+            applicationContext.AddRange(product, customer);
+            applicationContext.SaveChanges();
         }
     }
 }
